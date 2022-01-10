e9b87a1fa715e389ce2cc337ce373c7d7393c5cc
unta1337 <firstapple42@gmail.com>
2022-01-10 15:12:09 +0900

    p5: 애니메이션, 충돌 판정 예제 골격 추가

---
+++ P5/7_animations/script.js
@@ -0,0 +1,10 @@
+// p5.js 사용하기 예제 코드
+// 7. 애니메이션 예제
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(50);
+}
---
+++ P5/8_collision/script.js
@@ -0,0 +1,10 @@
+// p5.js 사용하기 예제 코드
+// 8. 충돌 판정 예제
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(50);
+}
