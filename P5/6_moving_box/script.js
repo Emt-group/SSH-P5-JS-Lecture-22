597e1e7ba86c759688f4be0645b3132be12601f5
unta1337 <firstapple42@gmail.com>
2022-01-13 15:39:05 +0900

    p5: 예제 단순화

--- P5/6_moving_box/script.js
+++ P5/6_moving_box/script.js
@@ -6,9 +6,7 @@
 
 var boxSize;        // 상자의 크기
 var boxX, boxY;     // 상자의 위치
-
-var boxSpeed;       // 상자의 속력
-var boxDirection;   // 상자의 방향
+var velX, velY;     // 상자의 속도
 
 function setup() {
     createCanvas(400, 400);
@@ -16,11 +14,12 @@ function setup() {
     // 상자 관련 변수 초기화
     boxSize = 20;
 
-    boxX = width / 4 - boxSize / 2;
-    boxY = height / 4 - boxSize / 2;
+    boxX = width / 4;
+    boxY = height / 4;
 
-    boxSpeed = 5;
-    boxDirection = 0;       // 상자의 방향, 0: 우하단, 1: 좌상단
+    // 초기 속도 설정
+    velX = 5;
+    velY = 2;
 }
 
 function draw() {
@@ -28,63 +27,7 @@ function draw() {
 
     rect(boxX, boxY, boxSize, boxSize);     // (boxX, boxY)위치에 boxSize 크기의 사각형을 그린다
 
-    // 상자 그리기의 함수화
-    // drawBox();
-
-    // 상자의 방향이 우하단이면
-    if (boxDirection === 0) {
-        // 상자의 위치를 boxSpeed만큼 조정
-        // +x, +y 방향이므로 우하단
-        boxX += boxSpeed;
-        boxY += boxSpeed;
-    }
-    
-    // 상자의 방향이 좌상단이면
-    else if (boxDirection === 1) {
-        // 상자의 위치를 boxSpeed만큼 조정
-        // -x, -y 방향이므로 좌상단
-        boxX -= boxSpeed;
-        boxY -= boxSpeed;
-    }
-
-    // 상자 이동의 함수화
-    // if (boxDirection === 0) {
-    //     moveBox(boxSpeed, boxSpeed);
-    // } else if (boxDirection === 1) {
-    //     moveBox(-boxSpeed, -boxSpeed);
-    // }
-
-    // 상자의 위치가 특정 좌표 이상이면 방향을 바꾼다 (우하단 -> 좌상단)
-    if (boxX >= width * (3 / 4) - boxSize / 2) {
-        boxDirection = 1;
-
-    }
-
-    // 상자의 위치가 특정 좌표 이상이면 방향을 바꾼다 (좌상단 -> 우하단)
-    else if (boxX <= width / 4 - boxSize / 2) {
-        boxDirection = 0;
-    }
-
-    // 방향 전환의 함수화
-    // switchBoxDirectionIfNeeded();
-}
-
-// 상자를 그려주는 함수
-function drawBox() {
-    rect(boxX, boxY, boxSize, boxSize);
-}
-
-// 상자의 위치를 amount만큼 움직이는 함수
-function moveBox(amountX, amountY) {
-    boxX += amountX;
-    boxY += amountY;
-}
-
-// 방향 전환이 필요하다면 상자의 방향을 전환하는 함수
-function switchBoxDirectionIfNeeded() {
-    if (boxX >= width * (3 / 4) - boxSize / 2) {
-        boxDirection = 1;
-    } else if (boxX <= width / 4 - boxSize / 2) {
-        boxDirection = 0;
-    }
+    // 상자 움직이기
+    boxX = boxX + velX;
+    boxY = boxY + velY;
 }
--- P5/7_edge/script.js
+++ P5/7_edge/script.js
@@ -1,14 +1,12 @@
 // p5.js 사용하기 예제 코드
 // 7. 모서리 예외 처리하기
 /* 
- * 상자가 화면 밖을 나갔을 때 반대변 화면으로 나올 수 있도록 한다.
+ * 상자가 화면 경계에 닿았을 때 방향을 반전할 수 있다.
  */
 
 var boxSize;        // 상자의 크기
 var boxX, boxY;     // 상자의 위치
-
-var boxSpeed;       // 상자의 속력
-var boxDirection;   // 상자의 방향
+var velX, velY;     // 상자의 속도
 
 function setup() {
     createCanvas(400, 400);
@@ -16,68 +14,31 @@ function setup() {
     // 상자 관련 변수 초기화
     boxSize = 20;
 
-    boxX = width / 4 - boxSize / 2;
-    boxY = height / 4 - boxSize / 2;
+    boxX = width / 4;
+    boxY = height / 4;
 
-    boxSpeed = 5;
-    boxDirection = 0;       // 상자의 방향, 0: 우하단, 1: 좌상단
+    // 초기 속도 설정
+    velX = 5;
+    velY = 2;
 }
 
 function draw() {
     background(50);
 
-    // 상자 그리기
-    drawBox();
+    rect(boxX, boxY, boxSize, boxSize);     // (boxX, boxY)위치에 boxSize 크기의 사각형을 그린다
 
     // 상자 움직이기
-    moveBox(boxSpeed, boxSpeed * 2);
+    boxX = boxX + velX;
+    boxY = boxY + velY;
 
-    // 상자가 오른쪽 벽면을 넘어서면
-    if (boxX > width) {
-        boxX = -boxSize;
-    }
-    
-    // 상자가 왼쪽 벽면을 넘어서면
-    else if (boxX + boxSize < 0) {
-        boxX = width;
+    // 박스가 벽에 닿았을 때 방향 바꾸기
+    // 왼쪽 또는 오른족 벽에 닿았을 때
+    if (boxX <= 0 || boxX > width) {
+        velX = -velX;
     }
 
-    // 상자가 아래쪽 벽면을 넘어서면
-    if (boxY > height) {
-        boxY = -boxSize;
-    }
-    
-    // 상자가 위쪽 벽면을 넘어서면
-    else if (boxY + boxSize < 0) {
-        boxY = height;
-    }
-
-    // 모서리 예외 처리하기 함수화
-    // handleBoxEdge();
-}
-
-// 상자를 그려주는 함수
-function drawBox() {
-    rect(boxX, boxY, boxSize, boxSize);
-}
-
-// 상자의 위치를 amount만큼 움직이는 함수
-function moveBox(amountX, amountY) {
-    boxX += amountX;
-    boxY += amountY;
-}
-
-// 상자가 모서리에 다다랐을 때의 예외 처리
-function handleBoxEdge() {
-    if (boxX > width) {
-        boxX = -boxSize;
-    } else if (boxX + boxSize < 0) {
-        boxX = width;
-    }
-
-    if (boxY > height) {
-        boxY = -boxSize;
-    } else if (boxY + boxSize < 0) {
-        boxY = height;
+    // 위쪽 또는 아래쪽 벽에 닿았을 때
+    if (boxY <= 0 || boxY > width) {
+        velY = -velY;
     }
 }
